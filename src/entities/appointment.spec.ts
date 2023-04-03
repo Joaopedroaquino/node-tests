ad2272c7dd89c28772a5bad66de0ccc5ee0c9acb
joaopedroaquino <aqerhack@gmail.com>
2023-04-03 20:10:15 -0300

    refactor create valid appointment test

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -2,10 +2,14 @@ import { expect, test } from 'vitest'
 import { Appointment } from './appointment'
 
 test('create an appointment', () => {
+    const startsAt = new Date()
+    const endsAt = new Date()
+
+    endsAt.setDate(endsAt.getDate() + 1)
     const appointment = new Appointment({
         customer: 'Any customer',
-        startsAt: new Date(),
-        endsAt: new Date()
+        startsAt,
+        endsAt
     });
     expect(appointment).toBeInstanceOf(Appointment);
     expect(appointment.customer).toEqual('Any customer');
